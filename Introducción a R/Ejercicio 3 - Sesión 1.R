6c873c16d1846211d83c3cd40110a8c88251e885
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-18 16:47:31 -0600

    str(param) retorna el tipo de dato y el valor del elemento que se le introduzca en los parametros

--- Introducción a R/Ejercicio 3 - Sesión 1.R
+++ Introducción a R/Ejercicio 3 - Sesión 1.R
@@ -3,4 +3,9 @@
 #crea una lista de los elementos que reciba como parametros
 (milista <- list(nombre = "Pepe", no.hijos = 3, edades.hijos = c(4, 7, 9)))
 
-
+#str(param)
+#str recibe como parametro una lista y te retorna
+#los elementos con sus valores
+#igual si recibe un numero retornara el tipo de dato
+#y el valor del elemento 
+str(milista)
