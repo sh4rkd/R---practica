3e3566dfbb348f8913a3c8cfd4792eca253c6a4f
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-17 23:18:18 -0600

    Utilizando el metodo typeof para ver la diferencia en class

--- Introducción a R/Ejercicio 1 - Sesión 1.R
+++ Introducción a R/Ejercicio 1 - Sesión 1.R
@@ -6,11 +6,21 @@
 (vector <- c(2,2))
 
 #class(parametro) <- metodo que retorna el tipo de dato
-class(var.number)
 class(var.hola)
+class(var.number)
 class(var.double)
 class(var.logical)
 class(vector)
 #Nota El vector retornara el tipo de dato de los
 #elementos que almacena
+
+#typeof(parametro) <- metodo que retorna tipo de dato
+typeof(var.hola)
+typeof(var.number)
+typeof(var.double)
+typeof(var.logical)
+typeof(vector)
+#Solo hay diferencia en double en class retorna numeric
+#y en typeof retorna double
+
 
