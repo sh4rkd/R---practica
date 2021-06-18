ee4fd4b293750e2950bdff9f36b6575bee370dc5
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-17 23:13:21 -0600

    Usando el metodo class para ver que tipo de dato es

--- Introducción a R/Ejercicio 1 - Sesión 1.R
+++ Introducción a R/Ejercicio 1 - Sesión 1.R
@@ -3,4 +3,14 @@
 (var.number <- 5L)
 (var.double <- 2.7076)
 (var.logical <- T)
-(vector <- c(1,2))
+(vector <- c(2,2))
+
+#class(parametro) <- metodo que retorna el tipo de dato
+class(var.number)
+class(var.hola)
+class(var.double)
+class(var.logical)
+class(vector)
+#Nota El vector retornara el tipo de dato de los
+#elementos que almacena
+
