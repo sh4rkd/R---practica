e68a854120a7fc5e465010ed77f8dc3e53842068
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-18 12:27:22 -0600

    accediendo a los elementos de la matriz - corregido

--- Introducción a R/Ejercicio 2 - Sesión 1.R
+++ Introducción a R/Ejercicio 2 - Sesión 1.R
@@ -4,15 +4,20 @@
 #matrix hecha por mi para comprobar teorias
 (m <- matrix(seq(1, 27, 3),3,3))
 
-#accediendo a los datos de la matriz
+#accediendo a los elementos de la matriz
 #matriz[fila,columna]
-#intercepta los datos de la n fila en la n columna
+#intercepta los elementos de la n fila en la n columna
 m[1,1]
 
 #matriz[,columna]
-#accede a los datos de la n columna
+#accede a los elementos de la n columna
 m[ ,1]
 
 #matriz[fila,]
-#accede a todos los datos de la n fila
+#accede a todos los elementos de la n fila
 m[1,]
+
+#matriz[,]
+#muestra toda la matriz debido a que ni la fila
+#ni la columna intercepta los elementos
+m[,]
