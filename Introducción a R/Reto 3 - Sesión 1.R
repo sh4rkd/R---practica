45be6ee46c58d36f078aa68a0f25c1c50d60b02f
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-19 17:31:49 -0600

    reto 3 - guardar los resultados en un dataframe

--- Introducción a R/Reto 3 - Sesión 1.R
+++ Introducción a R/Reto 3 - Sesión 1.R
@@ -4,11 +4,17 @@
 #rnorm(n)
 #genera un vector n cantidades de numeros aleatorios
 vector <- rnorm(44)
+cubo <- vector
+
 
 #2. Escribe un loop que eleve al cubo las 
 #primeras 15 entradas y les sume 12.
 for(i in 1:15){
-  vector[i]=(vector[i]**3)+12
+  cubo[i]=(vector[i]**3)+12
 }
 
+#3. Guarda el resultado en un data frame, 
+#donde la primera columna sea el número 
+#aleatorio y la segunda el resultado, nómbralo df.al.
+df.al <- data.frame(numero_aleatorio=vector,cubo_aleatorio=cubo)
 
