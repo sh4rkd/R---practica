f12061d68f61f8ce11120590011b59574d3b9842
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-19 16:28:10 -0600

    reto 2 - crear variable amazon.best leyendo csv de enlace

---
+++ Introducción a R/Reto 2 - Sesión 1.R
@@ -0,0 +1,8 @@
+#1. Almacénalo en un data frame que se llame amazon.best
+(amazon.best <- (read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-con-R-Santander/master/Sesion-01/Data/bestsellers%20with%20categories.csv")))
+
+
+
+
+
+
