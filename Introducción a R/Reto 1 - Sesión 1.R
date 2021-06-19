fc8b4d767f3e59b1b3bc0952e04708e0f54ab211
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-19 15:59:23 -0600

    write.csv(param1,param2) como crear un archivo con los datos del dataframe

--- Introducción a R/Reto 1 - Sesión 1.R
+++ Introducción a R/Reto 1 - Sesión 1.R
@@ -9,10 +9,20 @@
 #objeto que se obtiene
 dim(netflix)
 
-#Obtener los títulos que se estrenaron después 
+#3. Obtener los títulos que se estrenaron después 
 #del 2015. Almacenar este df en una variable 
 #llamada net.2015 (Hint: primero podrías filtrar 
 #solo aquellas filas del data frame que corresponden 
 #a estrenos después del 2015, por ejemplo, 
 #dataframe[dataframe$columna > 2015, ])
 (net.2015 <- netflix[netflix$release_year > 2015,])
+
+#4. Escribir los resultados en un archivo .csv 
+#llamado res.netflix.csv (Hint: consulta la 
+#función write.csv)
+write.csv(net.2015,"netflix-2016-2020.csv")
+
+#write.csv(param1,param2)
+#recibe el dataframe como primer parametro
+#y el segundo parametro es el nombre del archivo
+#con la extensión con la que debe almacenarlo.
