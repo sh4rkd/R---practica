e34f968e1423406655cd01c35ba72a2227124589
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-19 16:18:18 -0600

    library(ggplot2) - cargando paquetes

--- Introducción a R/Ejercicio 5 - Sesión 1.R
+++ Introducción a R/Ejercicio 5 - Sesión 1.R
@@ -2,3 +2,8 @@
 #instala paquetes, dependiendo el nombre que reciba
 #como parametro es el paquete que te instalara
 install.packages("ggplot2")
+
+#library(ggplot2)
+#carga el paquete de la libreria
+#que recibe como parametro
+library(ggplot2)
