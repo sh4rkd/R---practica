4241e0219132a9b28e8030331ee3521840b7fd66
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-19 15:50:20 -0600

    Reto 1 - Leer el csv y almacenarlo a una variable llamada 'netflix'.

---
+++ Introducción a R/Reto 1 - Sesión 1.R
@@ -0,0 +1,6 @@
+#1. Leer el archivo "netflix_titles.csv" 
+#desde Github (https://raw.githubusercontent.com
+#/beduExpert/Programacion-R-Santander-2021/main
+#/Sesion-01/Data/netflix_titles.csv), 
+#almacenarlo en un df llamado netflix
+(netflix <- read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-R-Santander-2021/main/Sesion-01/Data/netflix_titles.csv"))
