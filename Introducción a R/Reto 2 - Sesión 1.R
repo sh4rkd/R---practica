ba682c6a294f02d347e89f41c7fd6ae71ce40997
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-19 16:33:36 -0600

    crear una variable llamada tAmazon que almacene la transpuesta del dataframe de amazon.best

--- Introducción a R/Reto 2 - Sesión 1.R
+++ Introducción a R/Reto 2 - Sesión 1.R
@@ -1,8 +1,10 @@
 #1. Almacénalo en un data frame que se llame amazon.best
 (amazon.best <- (read.csv("https://raw.githubusercontent.com/beduExpert/Programacion-con-R-Santander/master/Sesion-01/Data/bestsellers%20with%20categories.csv")))
 
-
-
-
+#2. Calcula el data frame transpuesto, 
+#asígnale el nombre de tAmazon y conviértelo 
+#en un data frame (Hint: consulta la ayuda sobre 
+#las funciones t y as.data.frame)
+(tAmazon <- t(as.data.frame(amazon.best)))
 
 
