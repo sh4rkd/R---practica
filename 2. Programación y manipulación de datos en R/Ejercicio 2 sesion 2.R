b4fc5c4676343f30b67e979eebd604aeb263cbf2
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-22 11:44:35 -0600

    Creando un metodo

--- 2. Programación y manipulación de datos en R/Ejercicio 2 sesion 2.R
+++ 2. Programación y manipulación de datos en R/Ejercicio 2 sesion 2.R
@@ -4,3 +4,16 @@
 #en el parametro
 View(iris)
 
+#Creando un metodo
+#nombre <- function(param){
+#   lo que debe realizar
+#}
+moda <- function(vector){
+  f.abs <- table(vector) # frecuencias absolutas
+  max.f.abs <- max(f.abs) # obtenemos la máxima frecuencia absoluta
+  pos.max <- which(f.abs == max.f.abs) # posición(es) de la(s) máxima(s) frecuencia(s) absoluta(s)
+  print("La(s) moda(s) es(son): ")
+  print(names(f.abs[pos.max]))
+  paste("Con una frecuencia de: ", unique(f.abs[pos.max]))
+}
+
