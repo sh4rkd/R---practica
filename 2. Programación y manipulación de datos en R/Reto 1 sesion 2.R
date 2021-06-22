448073cea6e1314522b0d9a600ec9f9398c27bec
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-22 11:36:07 -0600

    reto 1 sesion 2 - calcular media, mediana y la moda

---
+++ 2. Programación y manipulación de datos en R/Reto 1 sesion 2.R
@@ -0,0 +1,10 @@
+set.seed(134)
+x <- round(rnorm(1000, 175, 6), 1)
+library(DescTools)
+
+#1. Calcule, la media, mediana y moda de los 
+#valores en x
+mean(x) #media
+median(x) #mediana
+Mode(x) #moda
+
