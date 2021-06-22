318a4d83fa425e46349f6c85163633591f1d22da
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-22 12:25:58 -0600

    rbind(param) - agregando filas con datos

--- 2. Programación y manipulación de datos en R/Ejercicio 4 sesion 2.R
+++ 2. Programación y manipulación de datos en R/Ejercicio 4 sesion 2.R
@@ -4,3 +4,9 @@ cbind(1:10, 11:20, 21:30)
 cbind(1:10, matrix(11:30, ncol =2))
 cbind(data.frame(x = 1:10, y = 11:20), z = 21:30)
 
+#rbind(param)
+#agregando filas con datos
+df1 <- data.frame(x = 1:5, y = 6:10, z = 16:20)
+df2 <- data.frame(x = 51:55, y = 101:105, z = 151:155)
+df1; df2
+rbind(df1, df2)
