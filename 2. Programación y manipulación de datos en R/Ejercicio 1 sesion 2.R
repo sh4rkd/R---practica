6c6e76fab57dc67c4c440892a6f40cee469962bb
Sh4rkD <20006132@es.uveg.edu.mx>
2021-06-22 11:27:50 -0600

    quantile(vector,rango) retorna los cuartiles

--- 2. Programación y manipulación de datos en R/Ejercicio 1 sesion 2.R
+++ 2. Programación y manipulación de datos en R/Ejercicio 1 sesion 2.R
@@ -8,3 +8,18 @@ mean(x)
 #median
 #retorna la mediana del vector
 median(x)
+
+library(DescTools)
+
+Mode(x)
+
+x <- c(29, 13, 62, 4, 63, 96, 1, 90, 50, 46)
+
+#quantile(vector,rango)
+#retorna los cuartiles del vector dependiendo los rangos
+quantile(x, 0.25) # cuantil del 25%
+quantile(x, c(0.25,0.50,0.75)) # Cuartiles
+quantile(x, seq(0.1,0.9, by = 0.1)) # Deciles
+
+
+
